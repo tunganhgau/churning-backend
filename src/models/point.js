514f9ed850e6772557409b6bd5ae86d11618e2d8
Anh Nguyen <tunganhgau@gmail.com>
2019-04-02 22:18:25 -0400

    init setup

--- src/models/point.js
+++ src/models/point.js
@@ -1,25 +1,27 @@
 import mongoose from 'mongoose';
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId;
 
 let Point = new Schema({
-    name: {
-        type: String
-    },
-    point: {
-        type: Number 
-    },
-    type: {
-        type: String
-    },
-    accountNumber: {
-        type: String,
-        default: ''
-    },
-    expiration: {
-        type: Date,
-        default: new Date()
-    }
+  name: {
+    type: String
+  },
+  point: {
+    type: Number
+  },
+  type: {
+    type: String
+  },
+  accountNumber: {
+    type: String,
+    default: ''
+  },
+  expiration: {
+    type: Date,
+    default: new Date()
+  },
+  id: ObjectId
 });
 
 export default mongoose.model('Point', Point);
